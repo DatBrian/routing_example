f46d123169dcc1135210e0772cb13d310e1dc462
DatBrian <vikard.2005@hotmail.com>
2023-07-01 00:30:36 -0500

    feat: :sparkles: Nuevas características y secciones para ejemplificar el routing

---
+++ src/views/ContactView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Sección de Contacto</h1>
+</template>
---
+++ src/views/PostView.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Sección de Post elemento: {{ postId }}</h1>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+let postId = ref();
+
+onMounted(() => {
+    const route = useRoute();
+    postId.value = route.params.id;
+})
+</script>
+
+<style scoped lang="scss"></style>
